96165ea2db562a7adfec62bebc3df52a0866a0b6
Gerald Hagan <gerald.hagan@mobivs.com>
2022-10-05 09:59:42 +0000

    rock paper scissr game complet

--- .vscode/rock_paper_scissor.dart
+++ .vscode/rock_paper_scissor.dart
@@ -1,7 +1,34 @@
 import 'dart:io';
 
+import 'dart:math';
+
+enum Move { rock, paper, scissors }
+
 void main() {
-  stdout.writeln('Rock,Paper, Scissor?! (R,P,S)');
-  final input = stdin.readLineSync();
-  print('You have entered $input');
+  final rng = Random();
+  while (true) {
+    stdout.writeln('Rock,Paper, Scissor?! (R,P,S)');
+    final input = stdin.readLineSync();
+    if (input == 'R' || input == "P" || input == 'S') {
+     
+      var playerMove;
+
+      print('you palyed:$playerMove');
+      
+      if (input == 'r') {
+        playerMove = Move.rock;
+      } else if (input == 'p') {
+        playerMove = Move.paper;
+      } else {
+        playerMove == Move.scissors;
+      }
+      final random = rng.nextInt(3);
+      final aiMove = Move.values[random];
+      if (playerMove == Move.rock && aiMove =Move.scissors || playerMove ==Move.scissors && aiMove == Move.paper){
+        print('Player wins!!!')
+      }else {
+          print('You Lose');
+      }
+    }
+  }
 }
